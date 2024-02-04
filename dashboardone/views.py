1cecc507e4647b4c6982afd7b4e384ff82a25d9b
limoo <limoodrice@gmail.com>
2024-02-04 17:03:34 +0300

    Added a basic view to render analytics and reports sections

--- dashboardone/views.py
+++ dashboardone/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"dashboardone/home.html")
+
+def analytics(request):
+    return render(request,"dashboardone/analytics.html")
+
+def reports(request):
+    return render(request, "dashboardone/reports.html")
